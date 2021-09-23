1f1b5f43cc35beea038ae3086da914cef38da095
Art Beckett <89126163+Bear1986@users.noreply.github.com>
2021-09-23 18:59:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+ 
+def pyramid(char, num) :
+  for i in range(1, num + 1):
+    print(char * i)
+
+pyramid("*", 2)
+
+print()
+
+for i in range(1, 3):
+  print("*" * i)
+
+print()
+
+for i in range(1, 6):
+  print("+" * i)  
+
+print()
+
+for i in range(1, 11):
+  print("x" * i)
+
+
+
